c4951f116ef5fb443a559bd62a046170fa434a14
Otavio <estudosprogotavio@gmail.com>
2025-04-14 12:38:11 -0300

    Adicionando os bean do controller de livros

---
+++ src/main/java/dev/Zerphyis/ClenArch/config/LivroConfig.java
@@ -0,0 +1,34 @@
+package dev.Zerphyis.ClenArch.config;
+
+import dev.Zerphyis.ClenArch.application.gateways.RepositorioDeLivros;
+import dev.Zerphyis.ClenArch.application.usecases.CriarLivros;
+import dev.Zerphyis.ClenArch.application.usecases.ListarLivros;
+import dev.Zerphyis.ClenArch.infra.gateways.LivrosEntityMapper;
+import dev.Zerphyis.ClenArch.infra.gateways.RepositorioLivrosJpa;
+import dev.Zerphyis.ClenArch.infra.persistance.LivrosRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LivroConfig {
+
+    @Bean
+    CriarLivros criarUsuario(RepositorioDeLivros repositorioDeUsuario){
+        return new CriarLivros(repositorioDeUsuario);
+    }
+
+    @Bean
+    ListarLivros listarUsuarios(RepositorioDeLivros repositorioDeUsuario){
+        return new ListarLivros(repositorioDeUsuario);
+    }
+
+    @Bean
+    RepositorioLivrosJpa criarRepositorioJpa(LivrosRepository repositorio, LivrosEntityMapper mapper){
+        return new RepositorioLivrosJpa(repositorio, mapper);
+    }
+
+    @Bean
+    LivrosEntityMapper retornaMapper(){
+        return new LivrosEntityMapper();
+    }
+}
