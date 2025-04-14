d759f627514bbc08496519eff1025cfec05dc779
Otavio <estudosprogotavio@gmail.com>
2025-04-14 09:49:57 -0300

    Commit inicial

---
+++ src/main/java/dev/Zerphyis/ClenArch/ClenArchApplication.java
@@ -0,0 +1,13 @@
+package dev.Zerphyis.ClenArch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClenArchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClenArchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/Zerphyis/ClenArch/ClenArchApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.Zerphyis.ClenArch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClenArchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
