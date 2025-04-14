c8e436bd2d4983d85f6ac9c1dd1381aaa5b90dc2
Otavio <estudosprogotavio@gmail.com>
2025-04-14 09:51:32 -0300

    Adicionando novos atributos a entidade livros

---
+++ src/main/java/dev/Zerphyis/ClenArch/domain/entitiys/Editora.java
@@ -0,0 +1,19 @@
+package dev.Zerphyis.ClenArch.domain.entitiys;
+
+public class Editora {
+private String nome;
+
+
+    public Editora(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+}
