ebfd16822e35f2b1c3fe90636a9ed4d21981bd9e
Otavio <estudosprogotavio@gmail.com>
2025-04-14 09:51:07 -0300

    Adicionando repositorio de livros

---
+++ src/main/java/dev/Zerphyis/ClenArch/application/gateways/RepositorioDeLivros.java
@@ -0,0 +1,11 @@
+package dev.Zerphyis.ClenArch.application.gateways;
+
+import dev.Zerphyis.ClenArch.domain.entitiys.livros.Livros;
+
+import java.util.List;
+
+public interface RepositorioDeLivros {
+    Livros cadastrarLivros(Livros livros);
+
+    List<Livros> listarTodos();
+}
