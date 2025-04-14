49552fcdf80bf1c9e09195caf1ab3b2f7365be8a
Otavio <estudosprogotavio@gmail.com>
2025-04-14 12:37:37 -0300

    Adicionando o record de entrada de livros

---
+++ src/main/java/dev/Zerphyis/ClenArch/domain/entitiys/records/LivrosDto.java
@@ -0,0 +1,6 @@
+package dev.Zerphyis.ClenArch.domain.entitiys.records;
+
+import dev.Zerphyis.ClenArch.domain.entitiys.Editora;
+
+public record LivrosDto(String titulo,String autor, Integer anoPublicacao) {
+}
