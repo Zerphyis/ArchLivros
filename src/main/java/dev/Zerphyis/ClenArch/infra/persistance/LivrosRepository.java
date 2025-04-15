d57f9215328153e8c33fa22da385e462ca3c5f7e
Otavio <estudosprogotavio@gmail.com>
2025-04-15 05:42:54 -0300

    Adicionando metodo findByTitulo em LivrosRepository

--- src/main/java/dev/Zerphyis/ClenArch/infra/persistance/LivrosRepository.java
+++ src/main/java/dev/Zerphyis/ClenArch/infra/persistance/LivrosRepository.java
@@ -2,5 +2,9 @@ package dev.Zerphyis.ClenArch.infra.persistance;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface LivrosRepository extends JpaRepository<LivroEntity,Long> {
+
+    Optional<LivroEntity> findByTitulo(String titulo);
 }
