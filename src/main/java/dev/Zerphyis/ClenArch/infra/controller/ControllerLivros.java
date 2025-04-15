0fe39872bb1a24a807a5c9866fca3ae40266c881
Otavio <estudosprogotavio@gmail.com>
2025-04-15 05:35:01 -0300

    Adicionando a classe atualizarLivros na ControllerLivros

--- src/main/java/dev/Zerphyis/ClenArch/infra/controller/ControllerLivros.java
+++ src/main/java/dev/Zerphyis/ClenArch/infra/controller/ControllerLivros.java
@@ -1,11 +1,14 @@
 package dev.Zerphyis.ClenArch.infra.controller;
 
+import dev.Zerphyis.ClenArch.application.usecases.AtualizarLivros;
 import dev.Zerphyis.ClenArch.application.usecases.CriarLivros;
 import dev.Zerphyis.ClenArch.application.usecases.ListarLivros;
 import dev.Zerphyis.ClenArch.domain.entitiys.livros.Livros;
 import dev.Zerphyis.ClenArch.domain.entitiys.records.LivrosDto;
 import dev.Zerphyis.ClenArch.domain.entitiys.records.LivrosDtoResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -14,10 +17,12 @@ import java.util.stream.Collectors;
 public class ControllerLivros {
     private final CriarLivros criarLivros;
     private final ListarLivros listarLivros;
+    private final AtualizarLivros atualizarLivros;
 
-    public ControllerLivros(CriarLivros criarLivros, ListarLivros listarLivros) {
+    public ControllerLivros(CriarLivros criarLivros, ListarLivros listarLivros, AtualizarLivros atualizarLivros) {
         this.criarLivros = criarLivros;
         this.listarLivros = listarLivros;
+        this.atualizarLivros=atualizarLivros;
     }
 
     @PostMapping
@@ -33,5 +38,15 @@ public class ControllerLivros {
         return listarLivros.bucarTodosLivros().stream()
                 .map(u -> new LivrosDtoResponse(u.getTitulo(), u.getAutor(), u.getAnoPublicacao(), u.getEditora()))
                 .collect(Collectors.toList());
+    }
+
+    @PutMapping("/{titulo}")
+    public LivrosDtoResponse atualizarLivro(@PathVariable String titulo, @RequestBody LivrosDto dto) {
+        Livros livroAtualizado = new Livros(dto.titulo(), dto.autor(), dto.anoPublicacao());
+        Livros livro = atualizarLivros.atualizarLivro(titulo, livroAtualizado);
+        if (livro == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Livro não encontrado");
+        }
+        return new LivrosDtoResponse(livro.getTitulo(), livro.getAutor(), livro.getAnoPublicacao(), livro.getEditora());
     }
 }
