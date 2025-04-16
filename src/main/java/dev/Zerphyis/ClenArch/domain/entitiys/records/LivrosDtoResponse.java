d98c31da8bbcbf8fc97a563b134f9ce056b3b737
Otavio <estudosprogotavio@gmail.com>
2025-04-16 12:38:48 -0300

    retirando a editora da resposta no record

--- src/main/java/dev/Zerphyis/ClenArch/domain/entitiys/records/LivrosDtoResponse.java
+++ src/main/java/dev/Zerphyis/ClenArch/domain/entitiys/records/LivrosDtoResponse.java
@@ -2,6 +2,5 @@ package dev.Zerphyis.ClenArch.domain.entitiys.records;
 
 import dev.Zerphyis.ClenArch.domain.entitiys.Editora;
 
-public record LivrosDtoResponse(String titulo, String autor, Integer anoPublicacao,
-                                Editora editora){
+public record LivrosDtoResponse(String titulo, String autor, Integer anoPublicacao){
 }
